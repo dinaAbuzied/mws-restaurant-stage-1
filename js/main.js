ddb24b76d693b5e2585f33dc90a5c44f3c337d41
Dina Abuzied <dina.abuzied@gmail.com>
2018-11-13 04:59:17 +0200

    added mapboxToken to js files

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
             scrollWheelZoom: false
         });
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-            mapboxToken: '<your MAPBOX API KEY HERE>',
+            mapboxToken: 'pk.eyJ1IjoiZGluYWFidXppZWQiLCJhIjoiY2pvZjN3c2xoMDBlejN3cWxjOGVqNXJpaiJ9.Vcw_qUkqv5bmgsbY3YRLCA',
             maxZoom: 18,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
